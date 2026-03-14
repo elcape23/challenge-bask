1a8c556c7cb5d10ef9dbbd3e38cabc352e30c272
Juanjo <calvoledesma91@gmail.com>
2026-03-14 02:25:59 -0300

    Styiling the main page

--- src/app/(docs)/prototype-desktop/page.tsx
+++ src/app/(docs)/prototype-desktop/page.tsx
@@ -14,16 +14,28 @@ export default function PrototypeDesktopPage() {
       </div>
       <div className="flex flex-1 flex-col items-center justify-center gap-10 max-w-[300px]">
         <h1 className="text-heading-06 font-medium text-text-neutral-default">
-          Please, open the prototype in mobile version
+          I’d recommend viewing the prototype on a mobile device
         </h1>
-        <Button
-          size="lg"
-          variant="primary"
-          appearance="outlined"
-          onClick={() => router.push("/foundations")}
-        >
-          Docs
-        </Button>
+        <div className="flex w-full max-w-[220px] flex-col gap-3">
+          <Button
+            size="lg"
+            variant="primary"
+            appearance="outlined"
+            className="w-full"
+            onClick={() => router.push("/")}
+          >
+            Back
+          </Button>{" "}
+          <Button
+            size="lg"
+            variant="primary"
+            appearance="filled"
+            className="w-full"
+            onClick={() => router.push("/prototype")}
+          >
+            Prototype
+          </Button>
+        </div>
       </div>
     </div>
   );
